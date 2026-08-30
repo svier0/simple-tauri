a9869a14e1b312b405df58925f301e7ac4266376
心衍 <vier@j7yx.com>
2026-08-30 23:48:18 +0800

    ensure_node+sh.rs 更新

--- src/utils/ensure/mod.rs
+++ src/utils/ensure/mod.rs
@@ -1,17 +1,19 @@
 
-use super::unzip_remote;
 use crate::utils::sh2rs::sh2rs;
+use crate::utils::sh2rs::try_quote;
+use indoc::indoc;
 
 use std::env::consts::{ARCH, OS};
 
 /// 安装node
-pub fn ensure_node(ver: &str,dir: &str) -> Result<(), String> {
-	if crate::simple_tray::resource_dir("server/nodejs/node.exe").is_file()
-		|| crate::simple_tray::resource_dir("server/nodejs/node").is_file(){
+pub fn ensure_node(ver: &str,node_dir: &str) -> Result<(), String> {
+	let node_dir = if node_dir.is_empty() { "server/nodejs" } else { node_dir };
+	let ver = if ver.is_empty() { "26.7.0" } else { ver };
+
+	if crate::simple_tray::resource_dir(&format!("{node_dir}/node.exe")).is_file()
+		|| crate::simple_tray::resource_dir(&format!("{node_dir}/node")).is_file(){
 		return Ok(());
 	}
-	let ver = if ver.is_empty() { "26.7.0" } else { ver };
-	let dir = if dir.is_empty() { "server/nodejs" } else { dir };
 	let extract_dir = format!(
 		"node-v{ver}-{}-{}",
 		match OS {
@@ -36,15 +38,49 @@ pub fn ensure_node(ver: &str,dir: &str) -> Result<(), String> {
 	);
 	let url = format!("https://nodejs.org/dist/v{ver}/{filename}");
 	// 下载并解压
-    sh2rs!("cd {}",crate::simple_tray::resource_dir("").to_string_lossy()).ok();
-    unzip_remote(&url,dir,&extract_dir)?;
-    sh2rs!("mkdir -p {}/bin",dir).ok();
-    sh2rs!("mkdir -p {}/cache",dir).ok();
+    sh2rs!("cd {}",crate::simple_tray::resource_dir("").to_string_lossy().replace("\\","/"))?;
+    super::unzip_remote(&url,node_dir,&extract_dir)?;
+    sh2rs!("mkdir -p {}/bin",node_dir).ok();
+    sh2rs!("mkdir -p {}/cache",node_dir).ok();
     sh2rs!("cd -").ok();
     Ok(())
 }
 
 /// 安装pnpm
-pub fn ensure_pnpm(_ver: &str,_dir: &str) -> Result<(), String> {
-	Ok(())
+pub fn ensure_pnpm(_ver: &str,node_dir: &str) -> Result<(), String> {
+	let node_dir = if node_dir.is_empty() { "server/nodejs" } else { node_dir };
+
+	if crate::simple_tray::resource_dir(&format!("{node_dir}/bin/pnpm")).is_file(){
+		return Ok(());
+	}
+
+	#[cfg(windows)]
+	let cmd = get_node_cmd("call npm install pnpm -g",node_dir);
+	#[cfg(not(windows))]
+	let cmd = get_node_cmd("npm install pnpm -g",node_dir);
+
+	sh2rs!("sh {}",try_quote!("{}",cmd))
+}
+
+/// 获取node环境变量
+pub fn get_node_cmd(cmd:&str,node_dir: &str) -> String {
+	let node_dir = if node_dir.is_empty() { "server/nodejs" } else { node_dir };
+	#[cfg(windows)]
+	let cmd_pre = indoc! {r#"
+		@echo off
+		set "NODE_HOME=<NODE_HOME>"
+		set "NPM_CONFIG_PREFIX=%NODE_HOME%/bin"
+		set "NPM_CONFIG_CACHE=%NODE_HOME%/cache"
+		set "PATH=%NODE_HOME%;%NODE_HOME%/bin;"
+	"#}.replace("<NODE_HOME>",&crate::simple_tray::resource_dir(node_dir).to_string_lossy().replace("\\","/"));
+	#[cfg(not(windows))]
+	let cmd_pre = indoc! {r#"
+		#!/bin/bash
+		NODE_HOME=<NODE_HOME>
+		NPM_CONFIG_PREFIX=$NODE_HOME/bin"
+		NPM_CONFIG_CACHE=$NODE_HOME/cache"
+		PATH=$NODE_HOME:$NODE_HOME/bin;"
+	"#}.replace("<NODE_HOME>",&crate::simple_tray::resource_dir(node_dir).to_string_lossy().replace("\\","/"));
+	let cmd = format!("{}\n{}",cmd_pre,cmd);
+	cmd
 }
--- src/utils/sh2rs/sh.rs
+++ src/utils/sh2rs/sh.rs
@@ -1,29 +1,48 @@
 
 /// 启动脚本
 pub fn sh(script: &str) -> Result<(), String> {
-    let child = spawn_in_dir(super::to_path(""),script)
-        .map_err(|e|format!("{}",e))?;
-    super::set_stdout_buffer(&format!("PID: {}",child.id()));
-    Ok(())
-}
+    #[cfg(windows)]
+    use std::os::windows::process::CommandExt;
 
-#[cfg(windows)]
-use std::os::windows::process::CommandExt;
+    let dir = super::to_path("");
 
-#[cfg(windows)]
-fn spawn_in_dir(dir: std::path::PathBuf, exec_cmd: &str) -> std::io::Result<std::process::Child> {
-    std::process::Command::new("cmd")
-        .args(["/C", exec_cmd])
+    #[cfg(windows)]
+    let out = std::process::Command::new("cmd")
+        .args(["/C", script])
         .current_dir(dir)
         .creation_flags(0x0800_0000)
-        .spawn()
-}
-
-#[cfg(not(windows))]
-fn spawn_in_dir(dir: std::path::PathBuf, exec_cmd: &str) -> std::io::Result<std::process::Child> {
-    std::process::Command::new("sh")
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
+        .output()
+        .expect("failed to execute");
+    #[cfg(not(windows))]
+    let out = std::process::Command::new("sh")
         .arg("-c")
-        .arg(exec_cmd)
+        .arg(script)
         .current_dir(dir)
-        .spawn()
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
+        .output()
+        .expect("failed to execute");
+
+    // 调试输出 始终为空
+    eprintln!("{}", String::from_utf8_lossy(&out.stdout));
+
+    super::set_stdout_buffer(&format!("{}",String::from_utf8_lossy(&out.stdout)));
+
+    let err = String::from_utf8_lossy(&out.stderr);
+    if !err.is_empty() {
+        return Err(err.to_string());
+    }
+
+    if !out.status.success() {
+        let code = out.status.code()
+            .map(|n| format!("exit code {n}"))
+            .unwrap_or_else(|| "terminated by signal".to_string());
+
+        let stderr = String::from_utf8_lossy(&out.stderr);
+        return Err(format!("{code}\nstderr:\n{stderr}"));
+    }
+
+    Ok(())
 }
